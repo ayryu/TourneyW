b9efff9ed11674388e749f5b29597edf6c085992
Alexander Ryu <alexryu1@gmail.com>
2020-01-21 19:12:45 -0500

    Added logic for collecting names from API request

--- application.py
+++ application.py
@@ -1,9 +1,20 @@
+import json
 import requests
 
-res = requests.get("https://api.kuroganehammer.com/api/characters/name/ganondorf?game=ultimate")
-
+def main():
+    characterNames = []
+    res = requests.get("https://api.kuroganehammer.com/api/characters")
     if res.status_code != 200:
         raise Exception("ERROR: The Get request was unsuccessful, please try again!")
+        
+    data = json.loads(res.text)
+    for element in data:
+        characterNames.append(element['Name'])
+
+    for name in characterNames:
+        print(name)
 
 if __name__ == "__main__":
     main()
+
+#python application.py
