33fe148c39069aba27bad35df7ea8b75649f00aa
Alexander Ryu <alexryu1@gmail.com>
2020-01-22 22:00:46 -0500

    Filtered moves by ability to autocancel

--- application.py
+++ application.py
@@ -2,14 +2,13 @@ import os
 
 from flask import Flask, render_template, request, session
 from flask_session import Session
-from models import *
 import json
 import requests
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 @app.route("/")
 def index():
@@ -17,29 +16,30 @@ def index():
     res = requests.get("https://api.kuroganehammer.com/api/characters")
     if res.status_code != 200:
         raise Exception("ERROR: The Get request was unsuccessful, please try again!")
-        
-    data = json.loads(res.text)
+
+    data = res.json()
     for element in data:
         fighterNames.append(element['Name'])
 
-    for name in fighterNames:
-        print(name)
-
     return render_template("index.html", fighterNames=fighterNames)
 
-@app.route("/<str:selectedFighter>")
+@app.route("/selectedFighter", methods=["POST"])
 def selectFighter():
     movelist = {}
+    autoCancelledMoves = {} #key=move name value=autocancelvalue
     selectedFighter = request.form.get("selectedFighter")
     response = requests.get("https://api.kuroganehammer.com/api/characters/name/" + selectedFighter + "/moves?expand=true")
     if response.status_code != 200:
         raise Exception("ERROR: The Get request was unsuccessful, please try again!")
     
-    frameData = json.loads(response.text)
-    #for listItem in frameData:
-        #movelist.append(listItem["AutoCancel"])
+    frameData = response.json()
+    for listItem in frameData:
+        movelist[listItem['Name']] = listItem['AutoCancel']
+    
+    for key in movelist:
+        if (movelist[key] == None or movelist[key] == "-"):
+            continue
+        autoCancelledMoves[key] = movelist[key]
+        
+    return render_template("character-page.html", autoCancelledMoves=autoCancelledMoves)
 
-    return render_template("character-page.html")
-
-
-#python application.py
